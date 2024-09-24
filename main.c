c905b0d68dbeeba7f7aa698397b69e680bf497a8
Cadu Souza <cadusouza14@gmail.com>
2024-09-24 20:39:58 -0300

    Added some cmd args, these are purely experimental

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define LINE_CHUNK_SIZE 128
 #define NUMS_MAX_LEN 1024
@@ -14,6 +15,37 @@ int main(int argc, char *argv[]) {
     int nums_len;
     char *raw_num;
     float parsed_num;
+
+    int opt;
+    int show_all = 0;
+    int show_arithmetic = 0;
+    int show_geometric = 0;
+    int show_harmonic = 0;
+    int show_weighted_average = 0;
+
+    if (argc < 2) {
+        show_all = 1;
+    } else {
+        while ((opt = getopt(argc, argv, "aghw")) != -1) {
+            switch (opt) {
+            case 'a':
+                show_arithmetic = 1;
+                break;
+            case 'g':
+                show_geometric = 1;
+                break;
+            case 'h':
+                show_harmonic = 1;
+                break;
+            case 'w':
+                show_weighted_average = 1;
+                break;
+            default:
+                fprintf(stderr, "Unknown option: %c\n", opt);
+                return 1;
+            }
+        }
+    }
 
     while ((line = read_line(stdin, LINE_CHUNK_SIZE))) {
         if (line->len == 0) {
@@ -29,10 +61,22 @@ int main(int argc, char *argv[]) {
             nums_len++;
         }
 
-        printf("%f ", arithmetic_mean(nums, nums_len));
-        printf("%f ", geometric_mean(nums, nums_len));
-        printf("%f ", harmonic_mean(nums, nums_len));
-        printf("%f ", weighted_arithmetic_mean(nums, nums_len));
+        if (show_arithmetic || show_all) {
+            printf("%f ", arithmetic_mean(nums, nums_len));
+        }
+
+        if (show_geometric || show_all) {
+            printf("%f ", geometric_mean(nums, nums_len));
+        }
+
+        if (show_harmonic || show_all) {
+            printf("%f ", harmonic_mean(nums, nums_len));
+        }
+
+        if (show_weighted_average || show_all) {
+            printf("%f", weighted_arithmetic_mean(nums, nums_len));
+        }
+
         printf("\n");
 
         free(line);
